d5eb25f540094b7a687e764336be7105c1857266
bill <2966231784@qq.com>
2024-02-07 21:48:35 +0800

    v0.0.0.1

--- Windows/DrawingBoard.py
+++ Windows/DrawingBoard.py
@@ -2,9 +2,14 @@ import tkinter as tk
 from tkinter import ttk
 
 
+queue = []
+position = [0]*2
+status = 1
+points=[]
 
 class Board ():
-    def __init__(self,handle) -> None:
+
+    def __init__(self, handle) -> None:
         # super().__init__()
         # self.board = handle.Tk()
         self.canvas = tk.Canvas(handle, width=500, height=500, bg="white")
@@ -13,7 +18,7 @@ class Board ():
         self.setColorandWidth()
         self.bindDevice()
 
-    def setClearBtn(self,handle):
+    def setClearBtn(self, handle):
         # 创建清除按钮
         clear_button = ttk.Button(
             handle, text="Clear", command=self.clear_canvas)
@@ -25,17 +30,35 @@ class Board ():
         self.pen_width = 2
 
     def bindDevice(self, device="<B1-Motion>", reset_device="<ButtonRelease-1>"):
+        
         # 绑定鼠标事件
         self.canvas.bind(device, self.paint)
         self.canvas.bind(reset_device, self.reset)
+        
 
+    def draw_points_and_lines(self,canvas, points):
+        
+        # 连线
+        for i in range(len(points) - 1):
+            x1, y1 = points[i]
+            x2, y2 = points[i + 1]
+            canvas.create_line(x1, y1, x2, y2, fill="black")
+        
     def paint(self, event):
-        print(f"x:{event.x},y:{event.y}")
+        # global queue
+        global points
+        
+        # print(f"x:{event.x},y:{event.y}")
         x1, y1 = (event.x - 0.01), (event.y - 0.01)
-        x2, y2 = (event.x + 0.01), (event.y + 0.01)
+        x2, y2 = (event.x + 0.1), (event.y + 0.1)
+        points.append((x1,y1))
+        # points.append((x2,y2))
+        # queue.append(f"{event.x}+{event.y}")
         self.canvas.create_oval(
             x1, y1, x2, y2, fill=self.pen_color, width=self.pen_width)
-        # self.canvas.update()
+        # self.canvas.create_line(event.x,event.y,event.x+5,event.y)
+        # self.draw_points_and_lines(self.canvas,points)
+        # points=[]
 
     def reset(self, event):
         self.canvas.create_line(
